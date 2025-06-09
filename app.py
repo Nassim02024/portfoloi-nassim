1f86a7bd95f90643190193af7e095e0210158bf2
Nassim02024 <nn1509789@gmail.com>
2025-06-09 22:01:02 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask , render_template , request , url_for , redirect
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+  return  render_template("index.html")
+
+
+@app.route("/work")
+def work():
+  return  render_template("work.html")
+
+
+@app.route('/about')
+def about():
+  return render_template('about.html')
+
+
+@app.route('/blog')
+def blog():
+  return render_template('blog.html')
+
+if __name__ ==  "main":
+  app.run(debug=True)
